e2b7f831455f9e5824ddfd0d3a2b4d78509b956a
Nikolay <efrem_nn@mail.ru>
2020-12-15 20:18:00 +0700

    Исправлен localStorage

--- js/main.js
+++ js/main.js
@@ -1,83 +1,79 @@
 let tasks = []
 
-if(localStorage.getItem('state') != undefined)
-  tasks = JSON.parse(localStorage.getItem('state'))
+if (localStorage.getItem('state') != undefined)
+    tasks = JSON.parse(localStorage.getItem('state')).map((el) => new Task(el.title, el.done))
 
 function localSave(save) {
-  localStorage.removeItem('state')
-  localStorage.setItem('state', JSON.stringify(save))
-  tasks = JSON.parse(localStorage.getItem('state'))
+    localStorage.removeItem('state')
+    localStorage.setItem('state', JSON.stringify(save))
 }
 
 function deleteTaskHandler() {
-  this.done = !this.done
-  renderTasks(tasks)
+    this.done = !this.done
+    localSave(tasks)
+    renderTasks(tasks)
 }
 
 function deletePushTask() {
-  tasks = tasks.filter((el) => el.title !== this.title)
-  localSave(tasks)
-  renderTasks(tasks)
+    tasks = tasks.filter((el) => el.title !== this.title)
+    localSave(tasks)
+    renderTasks(tasks)
 }
 
-function Task(title) {
-  this.title = title
-  this.done = false
+function Task(title, done = false) {
+    this.title = title
+    this.done = done
 
-  this.deleteTaskHandler = deleteTaskHandler
-  this.deletePushTask = deletePushTask
+    this.deleteTaskHandler = deleteTaskHandler
+    this.deletePushTask = deletePushTask
 }
 
 function getTaskElement(task) {
-  const containerEl = document.createElement('div')
-  const inputEl = document.createElement('input')
-  const titleEl = document.createElement('p')
-  const buttonEl = document.createElement('button')
+    const containerEl = document.createElement('div')
+    const inputEl = document.createElement('input')
+    const titleEl = document.createElement('p')
+    const buttonEl = document.createElement('button')
 
-  containerEl.classList.add('task-item')
-  inputEl.setAttribute('type', 'checkbox')
-  if (task.done) {
-    inputEl.setAttribute('checked', 'checked')
-    containerEl.classList.add('done')
-  }
-  titleEl.classList.add('task-item__title')
-  titleEl.textContent = task.title
-  buttonEl.classList.add('task-item__btn')
-  buttonEl.textContent = 'delete'
-  debugger
-  buttonEl.addEventListener('click', task.deletePushTask.bind(task))
+    containerEl.classList.add('task-item')
+    inputEl.setAttribute('type', 'checkbox')
+    if (task.done) {
+        inputEl.setAttribute('checked', 'checked')
+        containerEl.classList.add('done')
+    }
+    titleEl.classList.add('task-item__title')
+    titleEl.textContent = task.title
+    buttonEl.classList.add('task-item__btn')
+    buttonEl.textContent = 'delete'
+    buttonEl.addEventListener('click', task.deletePushTask.bind(task))
 
+    inputEl.addEventListener('click', task.deleteTaskHandler.bind(task))
 
-  inputEl.addEventListener('click', task.deleteTaskHandler.bind(task))
+    containerEl.appendChild(inputEl)
+    containerEl.appendChild(titleEl)
+    containerEl.appendChild(buttonEl)
 
-  containerEl.appendChild(inputEl)
-  containerEl.appendChild(titleEl)
-  containerEl.appendChild(buttonEl)
-
-  return containerEl
+    return containerEl
 }
 
 function renderTasks(tasks) {
-  const taskListEl = document.querySelector('#task-list')
-  taskListEl.textContent = ''
+    const taskListEl = document.querySelector('#task-list')
+    taskListEl.textContent = ''
 
-  tasks.forEach((el) => {
-    taskListEl.appendChild(getTaskElement(el))
-  })
+    tasks.forEach((el) => {
+        taskListEl.appendChild(getTaskElement(el))
+    })
 }
 
 function addTaskHandler() {
-  debugger
-  const inputEl = document.querySelector('#new-task')
+    const inputEl = document.querySelector('#new-task')
 
-  if (inputEl.value) {
-    tasks.push(new Task(inputEl.value))
-    localStorage.setItem('state', JSON.stringify(tasks))
-    renderTasks(tasks)
-    inputEl.value = ''
-  }
+    if (inputEl.value) {
+        tasks.push(new Task(inputEl.value))
+        localStorage.setItem('state', JSON.stringify(tasks))
+        renderTasks(tasks)
+        inputEl.value = ''
+    }
 }
-
 
 const addTaskBtnEl = document.querySelector('#add-task-btn')
 addTaskBtnEl.addEventListener('click', addTaskHandler)
